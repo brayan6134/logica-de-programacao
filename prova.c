b8d7b660c0be6a5d58808ae473e0713a093f8203
unknown <Brayan6134>
2025-08-21 17:24:08 -0300

    teste2

--- prova.c
+++ prova.c
@@ -5,25 +5,23 @@ double soma (double a, double b);
 double subtracao(double a,double b);
 double multiplicacao(double a,double b);
 double divisao(double a,double b);
-double raiz_quadrada(double a, double x, double novoX);
-int main() 
-{
+double raiz(double a);
+int fatorial(int a);
+int fatorial2(int a);
+int main() {
 	
 	int opcao = 0;
 	double result = 0.0;
 	double a = 0.0;
 	double b = 0.0;
+	int fat = 0;
+	int result2 = 0;
 	int exp = 0;
-	double x = 0.0;
-	double novox = 0.0;
 
-	
-	
 	printf("Bem vindo a minha calculadora 1.0\n");
 	printf("escolha uma opcao\n");
 	
-	
-do{
+	do{
 		printf("|----------------------------|\n");
 		printf("|0 - soma                    |\n");
 		printf("|1 - subtracao               |\n");
@@ -38,106 +36,113 @@ do{
 		printf("|10- equacao de segundo grau |\n");
 		printf("|11 - Sair                   |\n");
 		printf("|----------------------------|\n");
-		
+
 		scanf("%d",&opcao);
 
-		
-		switch(opcao)
-{
-		
-		case 0:
-		{
-			printf("Informe os numeros que deseja realizar a operacao:\n");
-			scanf("%lf",&a);
-			printf("Informe outro numero\n");
-			scanf("%lf",&b);
-			result = soma(a,b);
-			printf("O resultado e %.5lf\n",result);	
-		}
-		
-		break;
-		case 1:
-		{
-			printf("Informe os numeros que deseja realizar a operacao:\n");
-			scanf("%lf",&a);
-			printf("Informe outro numero\n");
-			scanf("%lf",&b);
-			result = subtracao(a,b);
-			printf("O resultado e %.5lf\n",result);	
-		}
-		
-		break;
-		case 2:
-		{
-			printf("Informe os numeros que deseja realizar a operacao:\n");
-			scanf("%lf",&a);
-			printf("Informe outro numero\n");
-			scanf("%lf",&b);
-			result = multiplicacao(a,b);
-			printf("O resultado e %.5lf\n",result);	
-		}	
-		
-		break;
-		case 3:
-	{
 
-			printf("Informe os numeros que deseja realizar a operacao:\n");
-			scanf("%lf",&a);
-			printf("Informe outro numero\n");
-			scanf("%lf",&b);		
-		if(b!=0)
-		{
-			result = divisao(a,b);
-			printf("O resultado e %.5lf\n",result);
-		}
-		else 
-		{	
-		printf("nao e possivel dividir por zero\n");	
-		}	
-	}
-		
-		break;
-		case 4:
-		{
+		switch(opcao) {
+			case 0: 
+				printf("Informe os numeros que deseja realizar a operacao:\n");
+				scanf("%lf",&a);
+				printf("Informe outro numero\n");
+				scanf("%lf",&b);
+				result = soma(a,b);
+				printf("O resultado do calculo e: %.5lf\n",result);	
+			break;
+			
+			case 1:
+				printf("Informe os numeros que deseja realizar a operacao:\n");
+				scanf("%lf",&a);
+				printf("Informe outro numero\n");
+				scanf("%lf",&b);
+				result = subtracao(a,b);
+				printf("O resultado do calculo e: %.5lf\n",result);	
+			break;
+			
+			case 2:
+				printf("Informe os numeros que deseja realizar a operacao:\n");
+				scanf("%lf",&a);
+				printf("Informe outro numero\n");
+				scanf("%lf",&b);
+				result = multiplicacao(a,b);
+				printf("O resultado do calculo e: %.5lf\n",result);	
+			break;
+			
+			case 3:
+				
+				printf("Informe os numeros que deseja realizar a operacao:\n");
+				scanf("%lf",&a);
+				printf("Informe outro numero\n");
+				scanf("%lf",&b);		
+				
+				if(b!=0) {
+					result = divisao(a,b);
+					printf("O resultado do calculo e: %.5lf\n",result);
+				} else  {	
+					printf("nao e possivel dividir por zero\n");	
+				}
+				
+			break;
+			case 4:	
 				printf("Informe a base:\n");
 				scanf("%lf",&a);
 				printf("Informe o expoente\n");
 				scanf("%d",&exp);
 				result = Potenciacao(a,exp);
-				printf("O resultado e: %.2lf\n",result);		
-		}
+				printf("O resultado do calculo e: %.5lf\n",result);		
+			break;
+			
+			case 5: 
+				printf("Informe um numero positivo\n");
+				scanf("%lf",&a);
+				if (a < 0)
+				{
+					printf("Erro: nao existe raiz quadrada real de numero negativo.\n");
+				}  
+				else  
+				{
+					result = raiz(a);
+					printf("o resultado do calculo e: %.5lf\n",a,result);
+				}
+				
 
-        break;
-		case 5:
-		{
-		printf("informe o numero positivo\n");
-		scanf("%lf",&a);
-		result = raiz_quadrada(a,x,novox);
-		printf("o valor de raiz quadrada de %.5lf e %.5lf/n",x,novox);	
+			break;
+			case 6:
+			printf("insira o numero\n");
+			scanf("%d",&fat);
+			result2 = fatorial(fat);
+			printf("o resultado do calculo e: %d\n",result2);
+			
+			break;
+			case 7:
+			printf("digite o numero para calcular\n");
+			scanf("%d",&fat);
+			result2 = fatorial2(fat);
+			printf("o resultado do calculo e: %d\n",result2);
+			
+			
 		}
 		
-		break;
-}
-		}while(opcao == 10);
-
-
-
+	}while(opcao != 11);
 
     
-    return 0;
+	return 0;
+		
 }
 
 double Potenciacao(double base, int exp)
 {
-	double result = 1.0;
+	double potencia = 1.0;
+
 	
 	for(int i = 0; i < exp;i++)
 	{
-		result = base*result;
+		potencia = base*potencia;
 	}
 	
-	return(result);
+	return(potencia);
 }
+
 double soma (double a, double b)
 {
 	
@@ -166,21 +171,46 @@ double divisao (double a, double b)
 	divisao = a/b;
 	return(divisao);
 }
-double raiz_quadrada(double a, double x, double novox)
+double raiz(double a)
 {
-    if (a < 0) 
+	double raiz = 0.0;
+	double x = 0.0;
+	x=a;
+	for (int c = 0; c < 1000; c++)
 	{
-        return -1;
+        raiz= 0.5 * (x + a/x);
+		x = raiz;
     }
+    return(raiz);
+	
+}
+int fatorial (int a)
+{
+	int fatorial = 1;
+	for(int c = 1; c <= a;c++){
+	fatorial = fatorial*c;	
+	}	
+	return (fatorial);
+		
+}
+int fatorial2 (int a)
+{
 
-    double x = a / 2.0; 
-    double novox;
-
-    for (int i = 0; i < 1000; i++) 
-	{
-        novox = 0.5 * (x + a / x);
-        x = novox;
-    }
-
-    return(raiz_quadrada);
-}	
+	int fatorial2 = 1;
+			
+		if(a%2 == 0)
+		{
+			for(int c = 2; c <= a;c=c+2)
+			{	
+				fatorial2 = fatorial2*c;
+			}
+		}
+		else
+		{	
+			for(int c = 1; c <= a;c=c+2)
+			{
+				fatorial2 = fatorial2*c;
+			}
+		}
+	return(fatorial2);
+}		
